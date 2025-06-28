1c4b8e7ff69ba76efa049d6d0f4d35bc2dd04eca
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-06-29 00:08:22 +0530

    Update layout.jsx

--- app/(main)/admin/layout.jsx
+++ app/(main)/admin/layout.jsx
@@ -5,7 +5,7 @@ import { ShieldCheck, AlertCircle, Users, CreditCard } from "lucide-react";
 import { PageHeader } from "@/components/page-header";
 
 export const metadata = {
-  title: "Admin Settings - MediMeet",
+  title: "Admin Settings - EduConnect",
   description: "Manage Mentors, Students, and platform settings",
 };
 
@@ -27,7 +27,7 @@ export default async function AdminLayout({ children }) {
         defaultValue="pending"
         className="grid grid-cols-1 md:grid-cols-4 gap-6"
       >
-        <TabsList className="md:col-span-1 bg-muted/30 border h-14 md:h-40 flex sm:flex-row md:flex-col w-full p-2 md:p-1 rounded-md md:space-y-2 sm:space-x-2 md:space-x-0">
+        <TabsList className="md:col-span-1 bg-muted/40 border border-white h-14 md:h-40 flex sm:flex-row md:flex-col w-full p-2 md:p-1 rounded-md md:space-y-2 sm:space-x-2 md:space-x-0">
           <TabsTrigger
             value="pending"
             className="flex-1 md:flex md:items-center md:justify-start md:px-4 md:py-3 w-full"
