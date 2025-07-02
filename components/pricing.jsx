b934253a40a2e929386d3b88aeda99736cd850e3
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-07-02 18:49:49 +0530

    Update pricing.jsx

--- components/pricing.jsx
+++ components/pricing.jsx
@@ -6,7 +6,7 @@ import { PricingTable } from "@clerk/nextjs";
 
 const Pricing = () => {
   return (
-    <Card className="border-emerald-900/30 shadow-lg bg-gradient-to-b from-emerald-950/30 to-transparent">
+    <Card className="border-purple-500/10 shadow-lg bg-gradient-to-b from-black-950/30 to-transparent">
       <CardContent className="p-6 md:p-8">
         <PricingTable
           checkoutProps={{
